24d882d7e1a97900e8f2707f5ef38c9d38210ae5
Jeremy Hindle <jeremy.hindle@ownbyte.com>
2013-08-16 12:44:25 +0100

    Added missing '#' in Handlebars Templating

--- LendLib.js
+++ LendLib.js
@@ -66,6 +66,29 @@ if (Meteor.isClient) {
   }
 
 
+  Template.list.items = function()
+  {
+    if(Session.equals('current_list', null))
+    {
+      return null;
+    }
+    else
+    {
+      var cats = lists.findOne({_id:Session.get('current_list')});
+      if(cats&&cats.items)
+      {
+        for(var i = 0; i<cats.items.length; i++)
+        {
+          var d = cats.items[i];
+          d.Lendee = d.LentTo ? d.LentTo : "free";
+          d.LendClass = d.LentTo ? "label-important" : "label-success";
+        }
+        return cats.items;
+      }
+    }
+  }
+
+
 } //------ Closing bracket for if(Meteor.isClient){}
 
 if (Meteor.isServer) {
